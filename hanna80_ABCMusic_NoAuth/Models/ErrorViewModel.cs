e5c63b74b629abef68f4df7dec5fe450a648f2fb
g1hanna <g1hanna@icloud.com>
2018-03-13 13:01:52 -0400

    Add project files.

---
+++ hanna80_ABCMusic_NoAuth/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace hanna80_ABCMusic_NoAuth.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
